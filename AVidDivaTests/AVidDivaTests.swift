c6bd246379efc43f8fd6e415fb8b9d6b9dd0061a
Mark Powell <Mark.Powell@jpl.nasa.gov>
2019-10-27 14:36:19 -0700

    Initial Commit

---
+++ AVidDivaTests/AVidDivaTests.swift
@@ -0,0 +1,34 @@
+//
+//  AVidDivaTests.swift
+//  AVidDivaTests
+//
+//  Created by Powell, Mark W (397M) on 10/27/19.
+//  Copyright © 2019 Powellware. All rights reserved.
+//
+
+import XCTest
+@testable import AVidDiva
+
+class AVidDivaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
